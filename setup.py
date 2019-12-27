2d8ffca5ae387e7c08207b76a0042101e95f5a66
Kristof <kristof@daja.hu>
2019-12-27 18:39:16 +0100

    Version bumped to 1.1.2:   *  Merge pull request #5 from guyeshet/master      removed no_translations because it's deprecated in Django 2.2.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-minio-backend',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License | Copyright (c) 2019 Kristof Daja',
