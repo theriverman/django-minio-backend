f53e6fbe0bbda5ead022a35a2294b3b26d0ed4e8
Kristof Daja <22156894+theriverman@users.noreply.github.com>
2025-01-04 23:47:46 +0100

    Refactoring for better STORAGES support
    * management command "is_minio_available" updated

--- django_minio_backend/management/commands/is_minio_available.py
+++ django_minio_backend/management/commands/is_minio_available.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from django_minio_backend.models import MinioBackend
+from django_minio_backend.models import MinioBackend, MinioBackendStatic
+from django.core.files.storage import storages
 
 
 class Command(BaseCommand):
@@ -9,15 +10,15 @@ class Command(BaseCommand):
         parser.add_argument('--silenced', action='store_true', default=False, help='No console messages')
 
     def handle(self, *args, **options):
-        m = MinioBackend()  # use default storage
         silenced = options.get('silenced')
-        self.stdout.write(f"Checking the availability of MinIO at {m.base_url}\n") if not silenced else None
-
-        available = m.is_minio_available()
-        if not available:
-            self.stdout.flush()
-            raise CommandError(f'MinIO is NOT available at {m.base_url}\n'
-                               f'Reason: {available.details}')
-
-        self.stdout.write(f'MinIO is available at {m.base_url}', ending='\n') if not silenced else None
+        for storage_name, storage_config in storages.backends.items():
+            if not storage_config["BACKEND"].startswith(MinioBackend.__module__):
+                continue
+            self.stdout.write(f"Checking MinIO availability for storage: {storage_name}\n") if not silenced else None
+            m: MinioBackend|MinioBackendStatic = storages.create_storage(storage_config)
+            if not (available := m.is_minio_available()):
+                self.stdout.flush()
+                raise CommandError(f'MinIO is NOT available at {m.base_url}\n'
+                                   f'Reason: {available.details}')
+            self.stdout.write(f'  * MinIO is available at {m.base_url} for {storage_name}', ending='\n') if not silenced else None
         self.stdout.flush()
