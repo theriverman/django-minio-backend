f3daba4a4e792680f4a0d0bf6ba550a164730fa2
Kristof Daja <22156894+theriverman@users.noreply.github.com>
2025-01-07 22:33:02 +0100

    settings.py corrections before going live: play.min.io for static

--- DjangoExampleProject/settings.py
+++ DjangoExampleProject/settings.py
@@ -153,10 +153,10 @@ STORAGES = {  # -- ADDED IN Django 5.1
     "staticfiles": {
         "BACKEND": "django_minio_backend.models.MinioBackendStatic",
         "OPTIONS": {
-            "MINIO_ENDPOINT": os.getenv("GH_MINIO_ENDPOINT", "localhost:9000"),  # NO EXTERNAL ENDPOINT FOR STATIC FILES
-            "MINIO_ACCESS_KEY": os.getenv("GH_MINIO_ACCESS_KEY", "minioadmin"),
-            "MINIO_SECRET_KEY": os.getenv("GH_MINIO_SECRET_KEY", "minioadmin"),
-            "MINIO_USE_HTTPS": bool(distutils.util.strtobool(os.getenv("GH_MINIO_USE_HTTPS", "false"))),
+            "MINIO_ENDPOINT": os.getenv("GH_MINIO_ENDPOINT", "play.min.io"),  # NO EXTERNAL ENDPOINT FOR STATIC FILES
+            "MINIO_ACCESS_KEY": os.getenv("GH_MINIO_ACCESS_KEY", "Q3AM3UQ867SPQQA43P2F"),
+            "MINIO_SECRET_KEY": os.getenv("GH_MINIO_SECRET_KEY", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"),
+            "MINIO_USE_HTTPS": bool(distutils.util.strtobool(os.getenv("GH_MINIO_USE_HTTPS", "true"))),
             "MINIO_REGION": os.getenv("GH_MINIO_REGION", "us-east-1"),
             "MINIO_URL_EXPIRY_HOURS": timedelta(days=1),  # Default is 7 days (longest) if not defined
             "MINIO_CONSISTENCY_CHECK_ON_START": True,
